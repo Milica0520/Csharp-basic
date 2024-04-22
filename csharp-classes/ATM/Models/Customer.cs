1b4b4c1c402c583c0e71400dcf6190b822241b91
Milica <dujakovic.milica3@gmail.com>
2024-04-22 10:57:38 +0200

    exercise ATM

---
+++ csharp-classes/ATM/Models/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            
+        }
+        public Customer(string FirstName, string LastName, string CardNo, string PinCode, double OwnedMoney)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.CardNo = CardNo;
+            this.PinCode = PinCode;
+            this.OwnedMoney = OwnedMoney;
+        }
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        public string CardNo { get; set; }
+
+        public string PinCode { get; set; }
+
+        public double OwnedMoney { get; set; }
+
+        public double tempMoneyVariaable { get; set; }
+
+
+        public void BalanceChecking()
+        {
+            Console.WriteLine($"Your current balance is: {string.Format("{0:C}",OwnedMoney)}");
+        }
+
+        public void CashWithdrawal( double tempMoneyVariaable)
+        {
+            if(OwnedMoney > tempMoneyVariaable)
+            {
+                Console.WriteLine($"You withdrew {string.Format("{0:C}",tempMoneyVariaable)}." +
+                    $"You have {string.Format("{0:C}", OwnedMoney - tempMoneyVariaable)} left on your account.\nThank you for using the ATM app.");
+            }
+            else
+            {
+                Console.WriteLine("You do not have enough money on your account.");
+            }
+        }
+
+
+        public void CashDeposition(double tempMoneyVariaable)
+        {
+               Console.WriteLine($"You deposited {string.Format("{0:C}", tempMoneyVariaable)}." +
+                   $"You have {string.Format("{0:C}", OwnedMoney + tempMoneyVariaable)} left on your account.\nThank you for using the ATM app.  ");
+        }
+
+    }
+}
---
+++ csharp-classes/ATM/Program.cs
@@ -0,0 +1,74 @@
+﻿using ATM.Models;
+using System.Reflection.Metadata;
+
+namespace ATM
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             
+
+
+            Customer customer1 = new Customer("Bob", "Bobsky", "1234-1234-1234-1234", "1111", 4325);
+            Customer customer2= new Customer("Tamara ", "Zdravkovic", "1111-2222-3333-4444", "2222", 8657.25);
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+
+            Console.WriteLine("Welcome to the ATM app \n Please enter your card numer: ");       
+            string cardNumInp = Console.ReadLine(); 
+
+            Console.WriteLine("Enter your pin code: ");
+            string pinCodeInp = Console.ReadLine();
+
+
+
+            bool customerFound = false;
+            Customer currCostumer = null;
+
+            foreach (Customer customer in customers)
+            {
+                if(customer.CardNo == cardNumInp && customer.PinCode == pinCodeInp)
+                {
+                    customerFound = true;
+                    Console.WriteLine($"Welcome {customer.FirstName} {customer.LastName}!");
+                    currCostumer = customer;
+                }
+            }
+
+            if(customerFound == false)
+            {
+                Console.WriteLine("You entered wrong card number or pin code.");
+                return;
+            }
+
+            Console.WriteLine("What would you like to do: \n1.Check Balance\n2.Cash Withdrawal\n3.Cash Deposit");
+            string optionInp = Console.ReadLine();
+
+           
+                switch (optionInp)
+            {
+                case "1":
+                    currCostumer.BalanceChecking();
+                     break;
+                case "2":
+                    Console.WriteLine("Enter amout to withdrawl:");
+                    double moneyInp = double.Parse(Console.ReadLine());
+                    currCostumer.CashWithdrawal(moneyInp);
+                     break;
+                case "3":
+                    Console.WriteLine("Enter amout to deposite:");
+                    double moneyInp3 = double.Parse(Console.ReadLine());
+                    currCostumer.CashDeposition(moneyInp3);
+                     break;
+                default:
+                    Console.WriteLine("That option is not available.");
+                     break;
+                }
+
+
+          
+
+        }
+    }
+}
