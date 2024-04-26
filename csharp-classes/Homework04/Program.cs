add48a659d698da5becd01d589aae7e441c7b7fd
Milica <dujakovic.milica3@gmail.com>
2024-04-26 22:08:26 +0200

    homework class 4 done

---
+++ csharp-classes/Homework04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Homework04
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter year of birth(yyyy):");
+            int yearInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter mounth of birth(MM):");
+            int mounthInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter day of birth(dd):");
+            int dayInput = int.Parse(Console.ReadLine());
+
+
+            DateTime myBirthDay = new DateTime(yearInput, mounthInput, dayInput);
+
+            DateTime currentDay = DateTime.Today;
+
+            TimeSpan difference = currentDay - myBirthDay;//TimeSpan vraca dane, sate, minute
+
+            double yearsD = difference.Days / 365.25;
+            int years = Convert.ToInt32(yearsD);
+
+            int remainingDays = difference.Days % 365;
+            int months = remainingDays / 30;
+
+            Console.WriteLine($"Your age is: {years} years and {months} months ");
+
+
+
+        }
+
+    }
+
+}
